551c17cc040c321f8cb8fe3944659de61c75d662
Kathleen Ly <kathleenly@Kathleens-MacBook-Air.local>
2022-01-20 22:15:21 +1100

    Add save and load files

--- directory.rb
+++ directory.rb
@@ -1,6 +1,28 @@
+def save_students
+  # open the file
+    file = File.open("students.csv", "w")
+  # iterate over the student array
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
+    file.close
+  end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+  
 # Empty array in instance variable for all methods to access
 @students = []
 
+# # input the students
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just return twice"
@@ -20,30 +42,38 @@ end
 
 def interactive_menu
   loop do # 4. repeat from step 1
-  # 1. print the menu and ask the user what to do
     print_menu
     process(gets.chomp)
   end
 end
-
+# 1. print the menu and ask the user what to do
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
+# show the students
 def show_students
   print_header
   print_student_list
   print_footer
 end
 
+# 2. read the input and save it into a variable
+# 3. do what the user has asked
 def process(user_input)
   case user_input
   when "1"
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -67,4 +97,5 @@ def print_footer
 end
 
 interactive_menu
+
 
